519757cf45061a13461773059e55ee490329a8b4
tcjansen <jansent@astro.columbia.edu>
2020-01-28 11:54:00 -0500

    Tests still need to be updated (aka these tests are kind of useless right now)

--- phasma/tests/test_phasma.py
+++ phasma/tests/test_phasma.py
@@ -11,25 +11,20 @@ from phasma import Kepler
 
 class TestKepler(object):
     def __init__(self):
-        kic = 10666592
-        period = 2.20473540 * u.day
-        transit_duration = 4.0398 * u.hr
-        transit_epoch = 54.358470 + 2454900 - 2454833
+        self.kic = 10666592
+        self.period = 2.20473540 * u.day
+        self.transit_duration = 4.0398 * u.hr
+        self.transit_epoch = 54.358470 + 2454900 - 2454833
 
-        self.hatp11b = Kepler(kic, period, transit_duration,
-                              transit_epoch, transit_at_0=True,
-                              transit_duration_buff=1.1,
-                              mask_primary=False)
+        self.hatp11b = Kepler(self.kic, self.period,
+                              self.transit_duration,
+                              self.transit_epoch)
 
     def test_get_raw_lightcurve(self):
-        time, flux, flux_err = (self.hatp11b._raw_time,
-                                self.hatp11b._raw_flux,
-                                self.hatp11b._raw_flux_err)
-        assert len(time) == 51324
-        assert len(flux) == len(time)
-        assert len(flux_err) == len(time)
 
-    def test_final_phasecurve(self):
-        phase, flux, flux_err = (self.hatp11b.phase,
-                                 self.hatp11b.flux,
-                                 self.hatp11b.flux_err)
+        raw_time, raw_flux, raw_flux_err = (self.hatp11b.raw_time,
+                                            self.hatp11b.raw_flux,
+                                            self.hatp11b.raw_flux_err)
+        assert len(raw_time) == 51324
+        assert len(raw_flux) == len(time)
+        assert len(raw_flux_err) == len(time)
